91afd3eb4a45fd4461134a2db1ba7215b972684e
Gonzalo <gfontest@gmail.com>
2021-02-26 23:27:14 +0100

    139
    John se mueve
    Push con posibles errores para 3os. Demasiados archivos raros que me pide subir

---
+++ LGS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 movement;
+
+    private Animator _animator;
+    private Rigidbody _rigidbody;
+
+    [SerializeField]
+    private float turnSpeed;
+
+    private Quaternion rotation = Quaternion.identity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        movement.Set(horizontal, 0, vertical);
+        /*
+         * Tenemos que normalizar el vector ya que puede ser que las distancias no sean iguales
+         * Al pulsar arriba obtenemos el vector (1, 0, 0)
+         * Si pulsamos arriba + derecha obtenemos el vector (1, 0, 1)
+         * En el primer caso la distancia es de 1 y en el segundo de raíz de 2. Son diferentes
+         */
+        movement.Normalize();
+        
+        
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        _animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, 
+            turnSpeed * Time.deltaTime, 0f);
+        
+        rotation = Quaternion.LookRotation(desiredForward);
+        
+    }
+
+    private void OnAnimatorMove()
+    {
+        // movement me da la dirección
+        //Este es el truco que se usa cuando la propia animación ya lleva movimiento.
+        //s = so + V*t       so + delta S
+        _rigidbody.MovePosition(_rigidbody.position + movement * _animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(rotation);
+    }
+}
